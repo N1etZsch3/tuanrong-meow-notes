4990ef54f314917aaa7f3907a7d9c7ea42f345bf
N1etZsch3-Savior <3566624043@qq.com>
2026-07-16 17:55:53 +0800

    chore: finalize development integration

---
+++ backend/alembic/versions/20260716_0016_merge_guest_and_meow_number_migrations.py
@@ -0,0 +1,19 @@
+"""merge guest and meow number migrations
+
+Revision ID: 20260716_0016
+Revises: 20260712_0015, 20260716_0015
+Create Date: 2026-07-16 17:44:09.617013
+"""
+
+revision = "20260716_0016"
+down_revision = ("20260712_0015", "20260716_0015")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
